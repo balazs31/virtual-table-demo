eb031bd814cc2542de255ce1cccc7657a596181f
Balazs Kendi <kendi.balazs41@gmail.com>
2020-02-11 15:52:57 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'virtual-table-demo';
+export class AppComponent implements OnInit {
+  tableData = [];
+  columns = [];
+  cdkStatus = false;
+  rowsNumber = 1000;
+  ngOnInit() {}
+
+  generateColumns(amount) {
+    const columns = [];
+    for (let i = 0; i < amount; i++) {
+      columns.push(i + 1);
+    }
+
+    this.columns = columns;
+  }
+
+  toggleCdk() {
+    this.cdkStatus = !this.cdkStatus;
+  }
+
+  clearData() {
+    this.tableData = [];
+  }
+  generateData() {
+    this.generateColumns(31);
+
+    const tableData = [];
+    for (let i = 0; i < this.rowsNumber; i++) {
+      tableData.push({
+        name: `Name ${i}`,
+        id: i,
+        columns: [...this.columns]
+      });
+    }
+    console.log(tableData.length);
+    this.tableData = tableData;
+  }
 }
